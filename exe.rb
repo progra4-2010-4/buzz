2d8955ca08442a50dfed36aeb7e64b6ab21dd27d
Gmafz7 <gralonzo7@gmail.com>
2010-10-15 11:53:27 -0600

    Se cambio el archivo ruby

--- exe.rb
+++ exe.rb
@@ -1,8 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 
+#get('/')do 
+#  send_file "index.html"
+#end
 
-get('/')do 
-  send_file "index.html"
-end
+get ('/'){erb :index, :layout=>false}
+get ('/home'){erb :home }
+get ('/Traffic'){erb :Traffic }
+get ('/Notes'){erb :Notes }
+get ('/FriendsFinder'){erb :FriendsFinder }
+get ('/developers'){erb :home }
 
