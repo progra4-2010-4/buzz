83708da9c21f0e8482755695437d1f20645ed39e
Gmafz7 <gralonzo7@gmail.com>
2010-12-03 15:29:14 -0600

    En blanco

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Buzz::Application.config.secret_token = '23e156bef6df74d8ed230acafe973446d9b961bf8816115b9f44ac4ab3d392af334d6969520be11291938d5eeeb5ceae08cf88573ddaa8305e1fe12957239b59'
